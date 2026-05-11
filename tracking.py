14ca25b861e397b2f56ea7c43aa9fb0fb4c62a0b
galaxyae <fredking352@gmail.com>
2026-05-11 09:42:39 +0300

    update

--- tracking.py
+++ tracking.py
@@ -466,7 +466,7 @@ def uganda_kor():
 
     # check
     increment = 100 / len(checkpoints)
-    progress = .2
+    progress = .5
     return render_template('track.html', item="", ui="", shipment=product, has_arrived=has_arrived, has_arrived_message=has_arrived_message, sender=sender, has_shipped=has_shipped, shipping_message=shipping_message, product=transit, receiver=reciever, amount=len(transit), map = unique, lines = lines, source = locations[0]['country'].title(), checkpoints=checkpoints, checkpoints_length=len(checkpoints), increment=increment, progress=progress, error=error, flight_progress=flight_progress, arrival_time=arrival_time, shipment_error=shipment_error)
 
 def uganda_italy():
